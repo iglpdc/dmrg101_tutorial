231c1b7bb4f61a83d7b6e5c4d64e8fc7c866c9a6
Ivan Gonzalez <iglpdc@gmail.com>
2012-12-01 15:51:53 -0500

    Fixing this file.

--- solutions/two_qbit_plot.py
+++ solutions/two_qbit_plot.py
@@ -5,20 +5,19 @@ The file must contain two columns of the same lenght. The first column
 will be the data in the x-axis, the second column the data in the y-axis.
 
 Usage:
-  two_qbit_system.py [--dir=DIR -o=FILE]
-  two_qbit_system.py -h | --help
+  two_qbit_plot.py <file>
+  two_qbit_plot.py -h | --help
 
 Options:
   -h --help       Shows this screen.
-  -f --file=FILE  Ouput file [default: two_qbit_entropies.dat]
-  --dir=DIR        Ouput directory [default: ./]
 
 """
 from docopt import docopt
 import matplotlib.pyplot as plt
+import os
 
 args = docopt(__doc__, version = 0.1)
-data_file = os.path.join(os.path.abspath(args['--dir']), args['--file'])
+data_file = os.path.abspath(args['<file>'])
 fin = open(data_file, 'r')
 x = []
 y = []
